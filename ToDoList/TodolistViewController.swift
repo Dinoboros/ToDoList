cab6cccb7028a8d3ecd7e036dc8b9cd62acf31ec
Méryl <meryl.valier@gmail.com>
2018-08-07 14:53:56 +0200

    Ajout une nouvelle catégorie

--- ToDoList/TodolistViewController.swift
+++ ToDoList/TodolistViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 
 class TodolistViewController: UITableViewController {
 
-    let itemArray = ["Learn Swift", "Get an iPhone", "Get a iOS developement job", "Be rich"]
+    var itemArray = ["Learn Swift", "Get an iPhone", "Get a iOS developement job", "Be rich"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,7 +34,7 @@ class TodolistViewController: UITableViewController {
     //MARK: - Table view delegate methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        let checkItem = tableView.cellForRow(at: indexPath)?.accessoryType
+        var checkItem = tableView.cellForRow(at: indexPath)?.accessoryType
         
         if checkItem == .checkmark {
             checkItem = .none
@@ -45,5 +45,31 @@ class TodolistViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
+    //MARK: - Ajouter des items
+    @IBAction func addNewItem(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Ajouter une catégorie", message: "", preferredStyle: .alert)
+        
+        alert.addTextField { (textfield) in
+            textfield.placeholder = "Votre nouvelle catégorie"
+            textField = textfield
+        }
+        
+        alert.addAction(UIAlertAction(title: "Ajoutez la catégorie", style: .default, handler: { (action) in
+            if textField.text! != "" {
+                self.itemArray.append(textField.text!)
+                self.tableView.reloadData()
+            } else {
+                print("Champ vide !")
+            }
+        }))
+        
+        present(alert, animated: true, completion: nil)
+        
+        
+    }
+    
 }
 
